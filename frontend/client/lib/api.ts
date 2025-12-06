6f618b43c80a4c083038918ae5779785dff70fb3
rahul74747474 <ghibli141525@gmail.com>
2025-12-06 10:18:57 +0530

    Update api.ts

--- frontend/client/lib/api.ts
+++ frontend/client/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:  "http://localhost:4000/api",
+  baseURL:  "https://fmdh.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
